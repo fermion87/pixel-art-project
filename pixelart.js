bb87fac5828b18c9b5afb09e12c696b549d78f4d
Fermion <74116015+fermion87@users.noreply.github.com>
2023-02-28 15:27:45 -0600

    Reorganized code into functions and fixed x,y directions when creating grid.

--- pixelart.js
+++ pixelart.js
@@ -1,27 +1,76 @@
-//generate 16x16 div grid
+////This script generate 16x16 div grid that functions like a pixel image edtior.
+
+//// GLOBAL VARS
+
 const container = document.querySelector('#container');
-container.style.display = 'flex';
-
 const row = document.createElement('div');
-row.classList.add('row');
-row.textContent = '';
-row.style.flexDirection = "row";
-
 const square = document.createElement('div');
-square.classList.add('square');
-square.textContent = 'This is a square.';
-square.style.aspectRatio = "1 / 1";   
-//col.style.flexDirection = "row";
 
-//add 16 columns to each row
-for(let i = 0; i < 16; i++){
-    row.appendChild(square.cloneNode(true)); 
+//// FUNCTIONS
+
+// setElements()
+/// creates required elements 
+function setElements(){
+    //assign properties to container
+    container.style.display = 'flex';
+    container.style.flexDirection = "column";
+
+    //assign properties to row
+    row.classList.add('row');
+    row.textContent = '';
+    row.style.display = 'flex';
+    row.style.flexDirection = "row";
+
+    //assign properties to square
+    square.classList.add('square');
+    square.textContent = '';
+    square.style.aspectRatio = "1 / 1"; 
+    square.style.width = "4vh"; 
+    square.style.borderWidth = "1px";
 }
 
-//add 16 rows to the container
-for(let i = 0; i < 16; i++){
-   container.appendChild(row.cloneNode(true)); 
+// createGrid() 
+///creates a n x n grid of squares
+function createGrid(x,y){
+    //add x squares to each row
+    for(let i = 0; i < x; i++){
+        row.appendChild(square.cloneNode(true)); 
+    }
+
+    //add y rows to the container
+    for(let i = 0; i < y; i++){
+        container.appendChild(row.cloneNode(true)); 
+    }
 }
+
+// applyListeners()
+/// adds mouse event listeners to squares for hover and drawing effects
+function applyListeners(){
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.style.background = 'blue';
+        });
+        square.addEventListener('mouseleave', () => {
+            square.style.background = 'white';
+        });
+    });
+}
+
+//// MAIN
+//set element properties
+setElements();
+
+//create a 20 x 20 grid
+createGrid(32,32);
+
+//apply event listeners for mouse effects
+applyListeners();
+
+
+
+
 
 
 
